4c8db6d59d139bb8d5d5ca3d97a07f4b76534a40
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-06-08 09:08:14 +0000

    Main

--- app/src/main/java/angel/number/calculator/App.java
+++ app/src/main/java/angel/number/calculator/App.java
@@ -4,11 +4,7 @@
 package angel.number.calculator;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        new Util().Exit("failure", "Text 123");
     }
 }
